5c8f1327834b6828db2e868b3f6bf36efbc50a9b
The Dom <ayimdomnic@outlook.com>
2016-09-04 15:56:53 +0300

    Company:class('protected & public methods. eloquent relations and massign')

--- app/Company.php
+++ app/Company.php
@@ -7,4 +7,46 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     //
+    protected $table = 'companies';
+
+    public $primaryKey = 'id';
+
+
+    //mass asinables
+
+    protected $fillable = [
+	    'name',
+	    'website_name',
+	    'slogan',
+	    'logo',
+	    'css_file',
+	    'phone_number',
+	    'cell_phone',
+        'address',
+        'theme',
+        'state',
+        'city',
+        'zip_code',
+        'email',
+        'contact_email',
+        'sales_email',
+        'support_email',
+        'status',
+        'twitter',
+        'facebook',
+        'facebook_app_id',
+        'description',
+        'keywords',
+        'about_us',
+        'refund_policy',
+        'privacy_policy',
+        'terms_of_service',
+
+
+    ];
+
+    public function features()
+    {
+    	return $this->hasMany('App/CompanyFeatures');
+    }
 }
